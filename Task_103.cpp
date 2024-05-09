6c8e483203782cbd0dc670bb715b91eef7111d60
Nasim-Khan_Milon <nasmkhanmilon@gmail.com>
2024-05-09 19:27:28 +0600

    Visual effect of collision between the two circle.

--- Task_103.cpp
+++ Task_103.cpp
@@ -4,7 +4,7 @@
 
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 700
-int R=50;
+int R=60;
 SDL_Window *win = NULL;
 SDL_Renderer *rend = NULL;
 bool gameIsRunning=false;
@@ -15,19 +15,19 @@ int Y=SCREEN_HEIGHT/2;
 int x=SCREEN_WIDTH/2;
 int y=SCREEN_HEIGHT-R;
 bool col=false;
-int r_color=255;
-int  b_color=0;
+int r_color=200;
+int  g_color=50;
 int  f=0;
 int c=0;
 
-int initializing(){
+int starting(){
 if(SDL_Init(SDL_INIT_VIDEO)!=0){
 std::cout<<"Error: SDL failed to initialize\n"<<"SDL Error:"<<" "<<SDL_GetError()<<'\n';
 return 0;
 }
 
 
- win = SDL_CreateWindow("Drawing Circle",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH,SCREEN_HEIGHT, 0);
+ win = SDL_CreateWindow("Collision between the two circle",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH,SCREEN_HEIGHT, 0);
 if (!win)
     {
        std::cout<<"Error: SDL failed to open window\n"<<"SDL Error:"<<" "<<SDL_GetError()<<'\n';
@@ -51,7 +51,8 @@ bool collision(int x,int y,int X,int Y)
 {
 int d;
 d=(x-X)*(x-X)+(y-Y)*(y-Y);
-if(d<=4*R*R) return true;
+d=SDL_sqrt(d);
+if(d<=2*R) return true;
 return false;
 }
 
@@ -59,14 +60,13 @@ return false;
 
 void update()
 {
- col=collision(x,y,X,Y);
- if(col) {
+ //col=collision(x,y,X,Y);
+ if(collision(x,y,X,Y)) {
     
-    r_color=0;b_color=255;
-    if(f==0){r_color=0;b_color=255;f=1;}
-    else {r_color=255;b_color=0;f=0;}
+    X=-R;r_color=50;g_color=200;
+    if(f==0){r_color=50;g_color=200;f=1;}
+    else {r_color=200;g_color=50;f=0;}
     
-    X-=4;
     }
 
 else
@@ -77,7 +77,7 @@ if(X>=SCREEN_WIDTH+R) X=0;
 
 
 }
-void draw_Circle( int centerX, int centerY, int radius) {
+void createCircle( int centerX, int centerY, int radius) {
     for (int x = -radius; x <= radius; x++) {
         for (int y = -radius; y <= radius; y++) {
             if (x*x + y*y <= radius*radius) {
@@ -88,51 +88,32 @@ void draw_Circle( int centerX, int centerY, int radius) {
 }
 
 
-
-void Draw()
-{
-
-    SDL_SetRenderDrawColor(rend, 0, 255, 0, 0);
-	SDL_RenderClear(rend);
-
-    update();
-    //if(c==1) backword();
-
-	SDL_SetRenderDrawColor(rend, r_color, 0,b_color, 0);
-    draw_Circle(X,Y,R);
-
-    SDL_SetRenderDrawColor(rend, b_color, 0,r_color, 0);
-    draw_Circle(x,y,R);
-	SDL_RenderPresent(rend);
-
-}
-
 void  game_loop()
 {
-  SDL_Event event;
+  SDL_Event e;
 
-  while(SDL_PollEvent(&event))
+  while(SDL_PollEvent(&e))
      {
-       if(event.type==SDL_QUIT) {gameIsRunning=false; break;}
-       else if (event.type == SDL_KEYDOWN) {
-                        if(event.key.keysym.sym==SDLK_UP) 
+       if(e.type==SDL_QUIT) {gameIsRunning=false; break;}
+       else if (e.type == SDL_KEYDOWN) {
+                        if(e.key.keysym.sym==SDLK_UP) 
                         {
                             y-=10;
                             if(y==R) {x=SCREEN_WIDTH/2;y=SCREEN_HEIGHT-R;}
                         }
-                        if(event.key.keysym.sym== SDLK_DOWN)
+                        if(e.key.keysym.sym== SDLK_DOWN)
                         {
                              y+=10;
                             if(y+R>SCREEN_HEIGHT) {x=SCREEN_WIDTH/2;y=SCREEN_HEIGHT-R;}
 
                         }
-                        if(event.key.keysym.sym== SDLK_LEFT)
+                        if(e.key.keysym.sym== SDLK_LEFT)
                         {
                             x-=10;
                             if(x==R) {x=SCREEN_WIDTH/2;y=SCREEN_HEIGHT-R;}
                              
                         }
-                        if(event.key.keysym.sym==SDLK_RIGHT)
+                        if(e.key.keysym.sym==SDLK_RIGHT)
                         {
                           
                             x+=10;
@@ -147,12 +128,22 @@ void  game_loop()
 
 int main(int argc,char *argv[])
 {
-gameIsRunning=initializing();
+gameIsRunning=starting();
 
 while(gameIsRunning)
 {
 	game_loop();
-    Draw();
+    SDL_SetRenderDrawColor(rend, 88, 198, 200, 0);
+	SDL_RenderClear(rend);
+
+    update();
+
+	SDL_SetRenderDrawColor(rend, r_color, g_color, 0, 0);
+    createCircle(X,Y,R);
+
+    SDL_SetRenderDrawColor(rend, g_color, r_color, 0, 0);
+    createCircle(x,y,R);
+	SDL_RenderPresent(rend);
 }
 
 SDL_DestroyWindow(win);
